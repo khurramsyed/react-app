243f54eb12e72daf09ce6048036d868b893c5266
khurramsyed <mahmood.khurram@gmail.com>
2026-01-22 13:48:47 +0000

    removing unused imports

--- src/components/LinkedInExperience.tsx
+++ src/components/LinkedInExperience.tsx
@@ -1,7 +1,5 @@
 // Build something like this: https://reactpractice.dev/exercise/build-the-linkedin-add-experience-form/
 
-import React from "react";
-
 const LinkedInExperience = () => {
   return <div>LinkedInExperience</div>;
 };
