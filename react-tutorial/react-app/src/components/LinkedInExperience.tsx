14a91a189ac366a8cfcd3dba4615384541b07905
khurramsyed <mahmood.khurram@gmail.com>
2026-01-22 13:45:31 +0000

    removing unnecessary folers

--- react-tutorial/react-app/src/components/LinkedInExperience.tsx
+++
@@ -1,9 +0,0 @@
-// Build something like this: https://reactpractice.dev/exercise/build-the-linkedin-add-experience-form/
-
-import React from "react";
-
-const LinkedInExperience = () => {
-  return <div>LinkedInExperience</div>;
-};
-
-export default LinkedInExperience;
